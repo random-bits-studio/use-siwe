d8533ed859e4ddbd96f32c2e5137d0916d52684f
AJ May <aj7may@gmail.com>
2023-01-03 18:45:07 -0600

    fix: change extension in tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/next/index.ts", "src/express/index.tsx"],
+  entry: ["src/index.ts", "src/next/index.ts", "src/express/index.ts"],
   splitting: false,
   sourcemap: true,
   dts: true,
