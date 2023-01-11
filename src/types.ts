14d23c7f9311da1f475bce9b3d6d25c000db1817
AJ May <aj7may@gmail.com>
2023-01-10 21:50:52 -0500

    fix: change sign in schema to accept siwe message object instead of string

--- src/types.ts
+++ src/types.ts
@@ -8,8 +8,23 @@ declare module "iron-session" {
   }
 }
 
+const siweMessageSchema = z.object({
+  domain: z.string(),
+  address: z.string(),
+  statement: z.string().optional(),
+  uri: z.string(),
+  version: z.string(),
+  chainId: z.number(),
+  nonce: z.string(),
+  issuedAt: z.string().optional(),
+  expirationTime: z.string().optional(),
+  notBefore: z.string().optional(),
+  requestId: z.string().optional(),
+  resources: z.array(z.string()).optional(),
+});
+
 export const signInRequestSchema = z.object({
-  message: z.string(),
+  message: siweMessageSchema,
   signature: z.string(),
 });
 
